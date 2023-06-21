ea68037d20243b2dfaa225e389b63f0b5fb59c3b
MatiCastagno <matiicastagno007@gmail.com>
2023-06-21 01:57:15 -0300

    cambios en generar.sh

--- generar.sh
+++ generar.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-source personas.sh
-echo "aca iria menu"
-
-REG_VALIDAR_CANT_IMG="^[0-9]+$"
+# Solicitar la cantidad de imágenes a generar
+read -p "Ingrese la cantidad de imágenes a generar: " cantidad_imagenes
 
 # Imprime un mensaje en caso que el usuario se equivoque 
 function string_error {
@@ -13,40 +11,38 @@ function string_error {
 	echo -e "------------------------\e[0m"
 	echo
 }
-# le pido al usuario la cantidad de imagenes que quiere generar
-read -p "Ingrese la cantidad de imágenes: " CANT_IMG
 
-# Valido que ingrese numeros unicamente
-if ! [ [ "$CANT_IMG" =~ $REG_VALIDAR_CANT_IMG ] & [ "$CANT_IMG" != 0 ] ]; then
+# Verificar que se proporcionó un número válido
+if ! [[ "$cantidad_imagenes" =~ ^[0-9]+$ ]]
+then
   string_error
   exit 1
 fi
 
-nombres= extraer_nombres
+# Lista de nombres de personas
+nombres=("Juan" "María" "Pedro" "Ana" "Luis" "Laura")
 
-# Directorio para guardar las imágenes generadas
-directorio_salida="../personas_generadas"
+# Directorio para almacenar las imágenes generadas
+directorio_salida="imagenes_generadas"
 
-# Crear el directorio de salida si no existe
+# Crear el directorio de salida
 mkdir -p "$directorio_salida"
 
 # Generar imágenes y asignar nombres de archivo al azar
 for ((i=1; i<=cantidad_imagenes; i++)); do
-  
-  # ACA DEBERIA ASIGNARSE EL NOMBRE CREADO ANTERIORMENTE
-  nombre_imagen=${nombres[i]}
+  # Obtener un nombre aleatorio de la lista de nombres
+  nombre_aleatorio=${nombres[$RANDOM % ${#nombres[@]}]}
 
-  # Genero una imagen usando la web
+  # Genero una imagen
   wget -O "$directorio_salida/$nombre_aleatorio.jpg" "https://source.unsplash.com/random/900%C3%97700/?person"
 
-  sleep 3
+  sleep 2
 done
 
-# Comprimo las imágenes generadas 
-tar -czvf $directorio_salida/imagenes_generadas.tar.gz "$directorio_salida"
+# Comprimo las imágenes generadas
+tar -czvf imagenes_generadas.tar.gz "$directorio_salida"
 
 # Genero el archivo de suma de verificación
 md5sum imagenes_generadas.tar.gz > suma_verificacion.txt
 
 echo "Imágenes generadas, comprimidas y suma de verificación generada."
-echo md5sum
