d84bed6936730af59697e7b8eb0792c9db64ae6d
lperuginiroldan <lperuginiroldan@invap.com.ar>
2023-06-22 21:53:20 -0300

    personas.sh mayusculas

--- personas.sh
+++ personas.sh
@@ -5,7 +5,7 @@ function extraer_nombres {
         wget -q https://raw.githubusercontent.com/fernandezpablo85/name_suggestions/master/assets/dict.csv -O dict.csv
 
         # Extraer solo los nombres sin los números utilizando grep y sed
-        NOM=$(grep -oE '^[A-Za-z ]+' dict.csv | sed 's/[0-9]*$//' | sed 's/ /_/g')
+        NOM=$(grep -oE '^[A-Za-z ]+' dict.csv | sed 's/[0-9]*$//' | sed -E 's/(^|_)([a-z])/\1\u\2/g' | sed -E 's/([a-zA-Z]+)/\u&/g' | tr -d '_ ')
         echo "$NOM"     
         return 0
 }
